5ae1251ea3ecce9831cf286abe6f25462e83b526
Rabeya Akter <rabeyariya251@gmail.com>
2025-04-21 09:23:25 +0600

    complete dashboard analytics

--- src/Components/Dashboard/SubscriptionGrowth/SubscriptionGrowth.jsx
+++ src/Components/Dashboard/SubscriptionGrowth/SubscriptionGrowth.jsx
@@ -1,90 +1,83 @@
 import { DatePicker } from "antd";
 import {
-    XAxis,
-    YAxis,
-    CartesianGrid,
-    Tooltip,
-    AreaChart,
-    Area,
-    ResponsiveContainer,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  AreaChart,
+  Area,
+  ResponsiveContainer,
 } from "recharts";
 import dayjs from "dayjs";
 import { useState } from "react";
 
 const SubscriptionGrowth = () => {
-    const [selectedYear, setSelectedYear] = useState(dayjs().year());
+  const [selectedYear, setSelectedYear] = useState(dayjs().year());
 
-    // Mock data
-    const mockData = [
-        { name: "Jan", earnings: 10 },
-        { name: "Feb", earnings: 20 },
-        { name: "Mar", earnings: 30 },
-        { name: "Apr", earnings: 70 },
-        { name: "May", earnings: 65 },
-        { name: "Jun", earnings: 40 },
-        { name: "Jul", earnings: 30 },
-        { name: "Aug", earnings: 45 },
-        { name: "Sep", earnings: 40 },
-        { name: "Oct", earnings: 60 },
-        { name: "Nov", earnings: 80 },
-        { name: "Dec", earnings: 90 },
-    ];
+  // Mock data
+  const mockData = [
+    { name: "Jan", earnings: 10 },
+    { name: "Feb", earnings: 20 },
+    { name: "Mar", earnings: 30 },
+    { name: "Apr", earnings: 70 },
+    { name: "May", earnings: 65 },
+    { name: "Jun", earnings: 40 },
+    { name: "Jul", earnings: 30 },
+    { name: "Aug", earnings: 45 },
+    { name: "Sep", earnings: 40 },
+    { name: "Oct", earnings: 60 },
+    { name: "Nov", earnings: 80 },
+    { name: "Dec", earnings: 90 },
+  ];
 
+  const onChange = (date, dateString) => {
+    setSelectedYear(dateString);
+  };
 
-
-
-
-    const onChange = (date, dateString) => {
-        setSelectedYear(dateString);
-    };
-
-    return (
-        <div className="mt-4 p-4">
-            <div className="bg-gray-50 rounded-lg shadow px-4 py-6">
-                {/* Header Section */}
-                <div className="flex flex-col md:flex-row items-start md:items-center justify-between gap-4">
-                    <h1 className="text-lg md:text-xl font-medium">
-                    Membership Growth
-                    </h1>
-                    <DatePicker
-                        onChange={onChange}
-                        defaultValue={dayjs(selectedYear, "YYYY")}
-                        format={"YYYY"}
-                        picker="year"
-                        className="w-full md:w-auto"
-                    />
-                </div>
-
-                {/* Chart Section */}
-                <div className="mt-6" style={{ height: "300px" }}>
-                    <ResponsiveContainer width="100%" height="100%">
-                        <AreaChart
-                            data={mockData}
-                            margin={{
-                                top: 10,
-                                right: 20,
-                                left: -10,
-                                bottom: 0,
-                            }}
-                        >
-                         
-                            <CartesianGrid strokeDasharray="3 3" />
-                            <XAxis dataKey="name" />
-                            <YAxis tickFormatter={(value) => `${value}`} />
-                            <Tooltip />
-                            <Area
-                                type="monotone"
-                                dataKey="earnings"
-                                stroke="#f4660e"
-                                fillOpacity={1}
-                                fill="#f4660e"
-                            />
-                        </AreaChart>
-                    </ResponsiveContainer>
-                </div>
-            </div>
+  return (
+    <div className="mt-4 p-4">
+      <div className="bg-gray-50 rounded-lg shadow px-4 py-6">
+        {/* Header Section */}
+        <div className="flex flex-col md:flex-row items-start md:items-center justify-between gap-4">
+          <h1 className="text-lg md:text-xl font-medium">Membership Growth</h1>
+          <DatePicker
+            onChange={onChange}
+            defaultValue={dayjs(selectedYear, "YYYY")}
+            format={"YYYY"}
+            picker="year"
+            className="w-full md:w-auto"
+          />
         </div>
-    );
+
+        {/* Chart Section */}
+        <div className="mt-6" style={{ height: "300px" }}>
+          <ResponsiveContainer width="100%" height="100%">
+            <AreaChart
+              data={mockData}
+              margin={{
+                top: 10,
+                right: 20,
+                left: -10,
+                bottom: 0,
+              }}
+            >
+              <CartesianGrid strokeDasharray="3 3" />
+              <XAxis dataKey="name" />
+              <YAxis tickFormatter={(value) => `${value}`} />
+              <Tooltip />
+              <Area
+                type="monotone"
+                dataKey="earnings"
+                stroke="#f4660e"
+                fillOpacity={1}
+                fill="#f4660e"
+              />
+            </AreaChart>
+          </ResponsiveContainer>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default SubscriptionGrowth;
