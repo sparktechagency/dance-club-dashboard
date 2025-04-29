0e1abc695269b201264cda9da07b7a5ad8a117a3
Rabeya Akter <rabeyariya251@gmail.com>
2025-04-29 11:51:05 +0600

    edit banner

--- src/Pages/ManageBanner/ManageBanner.jsx
+++ src/Pages/ManageBanner/ManageBanner.jsx
@@ -1,12 +1,41 @@
+import { useState } from "react";
 import { AllImages } from "../../assets/image/AllImages";
 import GoBackButton from "../../Components/Shared/GobackButton/GoBackButton";
+import { Form, Modal, Upload } from "antd";
+import { FaImage } from "react-icons/fa";
 
 const ManageBanner = () => {
+  const [isAddModalOpen, setIsAddModalOpen] = useState(false);
+  const [isEditModalOpen, setIsEditModalOpen] = useState(false);
+  const showModal = () => {
+    setIsAddModalOpen(true);
+  };
+  const handleAddModalClose = () => {
+    setIsAddModalOpen(false);
+  };
+  const handleOk = () => {
+    setIsAddModalOpen(false);
+  };
+  const handleEdit = () => {
+    setIsEditModalOpen(true);
+  };
+  const handleEditModalClose = () => {
+    setIsEditModalOpen(false);
+  };
+  const handlEditeOk = () => {
+    setIsEditModalOpen(false);
+  };
+  const onFinish = () => {};
+  const handleProfilePicUpload = () => {};
+
   return (
     <div>
-      <div>
+      <div className="flex justify-between items-center">
         <GoBackButton text={"Banner Managements"} />
-        <button className=" px-4 py-1 rounded-md bg-primary text-white">
+        <button
+          onClick={showModal}
+          className=" px-4 py-2 rounded-md bg-primary text-white"
+        >
           Add Banner
         </button>
       </div>
@@ -26,7 +55,7 @@ const ManageBanner = () => {
               <img src={AllImages.banner2} alt="" className="w-52 h-40 " />
             </div>
           </div>
-          <button className="text-xl font-bold px-4 py-1 rounded-md">
+          <button onClick={handleEdit} className="text-xl font-bold px-4 py-1 rounded-md">
             Edit
           </button>
         </div>
@@ -45,7 +74,7 @@ const ManageBanner = () => {
               <img src={AllImages.banner} alt="" className="w-52 h-40 p-3" />
             </div>
           </div>
-          <button className="text-xl font-bold px-4 py-1 rounded-md">
+          <button onClick={handleEdit}  className="text-xl font-bold px-4 py-1 rounded-md">
             Edit
           </button>
         </div>
@@ -64,11 +93,99 @@ const ManageBanner = () => {
               <img src={AllImages.banner2} alt="" className="w-52 h-40" />
             </div>
           </div>
-          <button className="text-xl font-bold px-4 py-1 rounded-md">
+          <button onClick={handleEdit} className="text-xl font-bold px-4 py-1 rounded-md">
             Edit
           </button>
         </div>
       </div>
+      {/* Add Banner */}
+      <Modal
+        title="Add New Course"
+        open={isAddModalOpen}
+        onOk={handleOk}
+        onCancel={handleAddModalClose}
+        footer={false}
+      >
+        <Form
+          onFinish={onFinish}
+          name="add-token"
+          initialValues={{ remember: false }}
+          layout="vertical"
+        >
+          <div className="w-full">
+            <Form.Item
+              name="title"
+              label={<p className=" text-md">Add Banner image</p>}
+            >
+              <div className="border border-dashed border-secondary p-5 flex justify-center items-center h-40">
+                <Upload
+                  showUploadList={false}
+                  onChange={handleProfilePicUpload}
+                  className=" "
+                >
+                  <FaImage className="text-secondary h-10 w-10" />
+                  <p className="text-secondary">Upload Image</p>
+                </Upload>
+              </div>
+            </Form.Item>
+          </div>
+
+          <Form.Item type="submit">
+            <div className="flex justify-center items-center gap-2">
+              <button className="px-6 py-2 rounded-md bg-primary text-white">
+                Save
+              </button>
+              <button className="px-6 py-2 rounded-md border border-primary text-primary">
+                cancel
+              </button>
+            </div>
+          </Form.Item>
+        </Form>
+      </Modal>
+      {/* Edit Banner */}
+      <Modal
+        title="Edit Banner"
+        open={isEditModalOpen}
+        onOk={handlEditeOk}
+        onCancel={handleEditModalClose}
+        footer={false}
+      >
+        <Form
+          onFinish={onFinish}
+          name="add-token"
+          initialValues={{ remember: false }}
+          layout="vertical"
+        >
+          <div className="w-full">
+            <Form.Item
+              name="title"
+              label={<p className=" text-md">Add Banner image</p>}
+            >
+              <div className="border border-dashed border-secondary p-5 flex justify-center items-center h-40">
+                <Upload
+                  showUploadList={false}
+                  onChange={handleProfilePicUpload}
+                  className=" "
+                >
+                  <FaImage className="text-secondary h-10 w-10" />
+                  <p className="text-secondary">Upload Image</p>
+                </Upload>
+              </div>
+            </Form.Item>
+          </div>
+
+          <Form.Item type="submit">
+            <div className="flex justify-center items-center gap-2">
+              <button className="px-6 py-2 rounded-md bg-primary text-white">
+                Save
+              </button>
+              <button className="px-6 py-2 rounded-md border border-primary text-primary">
+                cancel
+              </button>
+            </div>
+          </Form.Item>
+        </Form>
+      </Modal>
     </div>
   );
 };
